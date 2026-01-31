73d8cfbb2deb2a0091ac0440616c51ede40a2d13
Eeshu Narayan <eeshunarayan15@gmail.com>
2026-01-31 10:48:28 +0530

    implemented routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {AuthProvider} from "./contexts/AuthContext.tsx";
-import {BrowserRouter} from "react-router";
+import {BrowserRouter, Route, Routes} from "react-router";
+import RootLayout from './components/RootLayout.tsx';
+import { OAuthCallback } from './pages/OAuthCallback.tsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <BrowserRouter> {/* Add Router */}
-          <AuthProvider>
-              <App />
-          </AuthProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      {" "}
+      {/* Add Router */}
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<App />} />
+            <Route path="auth/callback" element={<OAuthCallback />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
